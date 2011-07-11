b749484eda1bf0c58e1e5a3c6d4c4cf49f2aa5dc
Glauber Costa <glommer@redhat.com>
2011-07-11 15:28:18 -0400

    sched: adjust scheduler cpu power for stolen time
    This patch makes update_rq_clock() aware of steal time.
    The mechanism of operation is not different from irq_time,
    and follows the same principles. This lives in a CONFIG
    option itself, and can be compiled out independently of
    the rest of steal time reporting. The effect of disabling it
    is that the scheduler will still report steal time (that cannot be
    disabled), but won't use this information for cpu power adjustments.
    
    Everytime update_rq_clock_task() is invoked, we query information
    about how much time was stolen since last call, and feed it into
    sched_rt_avg_update().
    
    Although steal time reporting in account_process_tick() keeps
    track of the last time we read the steal clock, in prev_steal_time,
    this patch do it independently using another field,
    prev_steal_time_rq. This is because otherwise, information about time
    accounted in update_process_tick() would never reach us in update_rq_clock().
    
    Signed-off-by: Glauber Costa <glommer@redhat.com>
    Acked-by: Rik van Riel <riel@redhat.com>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Tested-by: Eric B Munson <emunson@mgebm.net>
    CC: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
    CC: Anthony Liguori <aliguori@us.ibm.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>
    
    Conflicts:
    	kernel/sched/core.c
    
    Change-Id: Ie99a6039757b8e91c28d48ad4bdad6478ec3f8ad

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -839,8 +839,14 @@ EXPORT_SYMBOL_GPL(account_system_vtime);
 
 static void update_rq_clock_task(struct rq *rq, s64 delta)
 {
-	s64 irq_delta;
-
+/*
+ * In theory, the compile should just see 0 here, and optimize out the call
+ * to sched_rt_avg_update. But I don't trust it...
+ */
+#if defined(CONFIG_IRQ_TIME_ACCOUNTING) || defined(CONFIG_PARAVIRT_TIME_ACCOUNTING)
+	s64 steal = 0, irq_delta = 0;
+#endif
+#ifdef CONFIG_IRQ_TIME_ACCOUNTING
 	irq_delta = irq_time_read(cpu_of(rq)) - rq->prev_irq_time;
 
 	/*
@@ -863,12 +869,35 @@ static void update_rq_clock_task(struct rq *rq, s64 delta)
 
 	rq->prev_irq_time += irq_delta;
 	delta -= irq_delta;
+#endif
+#ifdef CONFIG_PARAVIRT_TIME_ACCOUNTING
+	if (static_branch((&paravirt_steal_rq_enabled))) {
+		u64 st;
+
+		steal = paravirt_steal_clock(cpu_of(rq));
+		steal -= rq->prev_steal_time_rq;
+
+		if (unlikely(steal > delta))
+			steal = delta;
+
+		st = steal_ticks(steal);
+		steal = st * TICK_NSEC;
+
+		rq->prev_steal_time_rq += steal;
+
+		delta -= steal;
+	}
+#endif
+
 	rq->clock_task += delta;
 
-	if (irq_delta && sched_feat(NONIRQ_POWER))
-		sched_rt_avg_update(rq, irq_delta);
+#if defined(CONFIG_IRQ_TIME_ACCOUNTING) || defined(CONFIG_PARAVIRT_TIME_ACCOUNTING)
+	if ((irq_delta + steal) && sched_feat(NONTASK_POWER))
+		sched_rt_avg_update(rq, irq_delta + steal);
+#endif
 }
 
+#ifdef CONFIG_IRQ_TIME_ACCOUNTING
 static int irqtime_account_hi_update(void)
 {
 	u64 *cpustat = kcpustat_this_cpu->cpustat;
@@ -903,12 +932,7 @@ static int irqtime_account_si_update(void)
 
 #define sched_clock_irqtime	(0)
 
-static void update_rq_clock_task(struct rq *rq, s64 delta)
-{
-	rq->clock_task += delta;
-}
-
-#endif /* CONFIG_IRQ_TIME_ACCOUNTING */
+#endif
 
 void sched_set_stop_task(int cpu, struct task_struct *stop)
 {
