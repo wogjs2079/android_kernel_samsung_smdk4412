422a9ea06c83a8b15efd5802ed34ff0c7bf3b100
Dan Ballard <dan@mindstab.net>
2011-10-31 17:11:20 -0700

    kernel/sysctl.c: add cap_last_cap to /proc/sys/kernel
    Userspace needs to know the highest valid capability of the running
    kernel, which right now cannot reliably be retrieved from the header files
    only.  The fact that this value cannot be determined properly right now
    creates various problems for libraries compiled on newer header files
    which are run on older kernels.  They assume capabilities are available
    which actually aren't.  libcap-ng is one example.  And we ran into the
    same problem with systemd too.
    
    Now the capability is exported in /proc/sys/kernel/cap_last_cap.
    
    [akpm@linux-foundation.org: make cap_last_cap const, per Ulrich]
    Signed-off-by: Dan Ballard <dan@mindstab.net>
    Cc: Randy Dunlap <rdunlap@xenotime.net>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Lennart Poettering <lennart@poettering.net>
    Cc: Kay Sievers <kay.sievers@vrfy.org>
    Cc: Ulrich Drepper <drepper@akkadia.org>
    Cc: James Morris <jmorris@namei.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Conflicts:
    	Documentation/sysctl/kernel.txt
    	kernel/sysctl.c
    
    Change-Id: I102161f7549c0f3042692d63ea3d708f5f47b828

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -57,6 +57,7 @@
 #include <linux/pipe_fs_i.h>
 #include <linux/oom.h>
 #include <linux/kmod.h>
+#include <linux/capability.h>
 
 #include <asm/uaccess.h>
 #include <asm/processor.h>
@@ -138,6 +139,7 @@ static int minolduid;
 static int min_percpu_pagelist_fract = 8;
 
 static int ngroups_max = NGROUPS_MAX;
+static const int cap_last_cap = CAP_LAST_CAP;
 
 #ifdef CONFIG_INOTIFY_USER
 #include <linux/inotify.h>
@@ -806,6 +808,13 @@ static struct ctl_table kern_table[] = {
                 .mode           = 0444,
                 .proc_handler   = proc_dointvec,
         },
+	{
+		.procname	= "cap_last_cap",
+		.data		= (void *)&cap_last_cap,
+		.maxlen		= sizeof(int),
+		.mode		= 0444,
+		.proc_handler	= proc_dointvec,
+	},
 #if defined(CONFIG_LOCKUP_DETECTOR)
         {
                 .procname       = "watchdog",
